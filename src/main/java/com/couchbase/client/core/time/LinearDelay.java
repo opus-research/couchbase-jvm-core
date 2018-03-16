933bb7bbfb7d5de4d4459eaffdf1b7b8f900242f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:25:17 +0000

    First commited as after_49298_rev1

--- src/main/java/com/couchbase/client/core/time/LinearDelay.java
+++ src/main/java/com/couchbase/client/core/time/LinearDelay.java
@@ -54,4 +54,15 @@ public class LinearDelay extends Delay {
         }
         return calc;
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("LinearDelay{");
+        sb.append("unit=").append(unit());
+        sb.append(", growBy=").append(growBy);
+        sb.append(", lower=").append(lower);
+        sb.append(", upper=").append(upper);
+        sb.append('}');
+        return sb.toString();
+    }
 }
