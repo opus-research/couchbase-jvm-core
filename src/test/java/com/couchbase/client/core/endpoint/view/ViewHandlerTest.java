a3245c26b6425a4f5af34d27948483d7e95e77df
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:23:38 +0000

    First commited as after_41725_rev1

--- src/test/java/com/couchbase/client/core/endpoint/view/ViewHandlerTest.java
+++ src/test/java/com/couchbase/client/core/endpoint/view/ViewHandlerTest.java
@@ -329,6 +329,16 @@ public class ViewHandlerTest {
     }
 
     @Test
+    public void shouldEncodeLongViewQueryRequestWithPOST() {
+        String keys = Resources.read("key_many.json", this.getClass());
+        String query = "keys=" + keys;
+        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, query , "bucket", "password");
+        channel.writeOutbound(request);
+        HttpRequest outbound = (HttpRequest) channel.readOutbound();
+        assertEquals(HttpMethod.POST, outbound.getMethod());
+    }
+
+    @Test
     public void shoulDecodeNotFoundViewQueryResponse() {
 
     }
