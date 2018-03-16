3ab37da1e43781765a0446a4b30c91be989a9d32
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:28:50 +0000

    First commited as before_66949_rev1

--- src/main/java/com/couchbase/client/core/endpoint/search/SearchHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/search/SearchHandler.java
@@ -138,7 +138,6 @@ public class SearchHandler extends AbstractGenericHandler<HttpObject, HttpReques
             } else if (request instanceof RemoveSearchIndexRequest) {
                 response = new RemoveSearchIndexResponse(body, status);
             } else if (request instanceof SearchQueryRequest) {
-                //TODO if more parsing is implemented, add a RawSearchQueryRequest similar to what was done in JVMCBC-357
                 response = new SearchQueryResponse(body, status);
             }
 
