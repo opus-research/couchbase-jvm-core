605cddb6736936d5d8b201962741491b43606317
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:26:54 +0000

    First commited as before_56859_rev1

--- src/main/java/com/couchbase/client/core/endpoint/dcp/DCPConnection.java
+++ src/main/java/com/couchbase/client/core/endpoint/dcp/DCPConnection.java
@@ -28,9 +28,6 @@ import com.couchbase.client.core.utils.UnicastAutoReleaseSubject;
 import rx.subjects.SerializedSubject;
 import rx.subjects.Subject;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -39,7 +36,7 @@ import java.util.concurrent.TimeUnit;
  * @author Sergey Avseyev
  */
 public class DCPConnection {
-    private static final Map<Integer, String> streamRegistry = new ConcurrentHashMap<Integer, String>();
+    private static final Map<Integer, String> streams = new ConcurrentHashMap<Integer, String>();
     /**
      * Counter for stream identifiers.
      */
@@ -48,7 +45,6 @@ public class DCPConnection {
     private final SerializedSubject<DCPRequest, DCPRequest> subject;
     private final String bucket;
     private volatile int totalReceivedBytes;
-    private List<Integer> streams = Collections.synchronizedList(new ArrayList<Integer>());
 
     public DCPConnection(final CoreEnvironment env, final String name, final String bucket) {
         this.name = name;
@@ -58,24 +54,14 @@ public class DCPConnection {
                 .toSerialized();
     }
 
+    public static int addStream(final String connectionName) {
+        int newStreamId = nextStreamId++;
+        streams.put(newStreamId, connectionName);
+        return newStreamId;
+    }
+
     public static String connectionName(final int streamId) {
-        return streamRegistry.get(streamId);
-    }
-
-    public int addStream(final String connectionName) {
-        int streamId = nextStreamId++;
-        streams.add(streamId);
-        streamRegistry.put(streamId, connectionName);
-        return streamId;
-    }
-
-    public void removeStream(final int streamId) {
-        streams.remove((Integer) streamId);
-        streamRegistry.remove(streamId);
-    }
-
-    public int streamsCount() {
-        return streams.size();
+        return streams.get(streamId);
     }
 
     public String name() {
