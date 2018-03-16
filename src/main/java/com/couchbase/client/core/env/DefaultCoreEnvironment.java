826ed708df3f316bb172b78af3fca91110061d4d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:29:28 +0000

    First commited as after_72365_rev7

--- src/main/java/com/couchbase/client/core/env/DefaultCoreEnvironment.java
+++ src/main/java/com/couchbase/client/core/env/DefaultCoreEnvironment.java
@@ -808,6 +808,26 @@ public class DefaultCoreEnvironment implements CoreEnvironment {
         return instanceCounter;
     }
 
+    @Override
+    public KeyValueServiceConfig kvServiceConfig() {
+        return KeyValueServiceConfig.create(kvEndpoints()); // TODO: fixme
+    }
+
+    @Override
+    public QueryServiceConfig queryServiceConfig() {
+        return QueryServiceConfig.create(0, 24); // TODO: fixme
+    }
+
+    @Override
+    public ViewServiceConfig viewServiceConfig() {
+        return ViewServiceConfig.create(0, 24); // TODO: fixme
+    }
+
+    @Override
+    public SearchServiceConfig searchServiceConfig() {
+        return SearchServiceConfig.create(0, 24); // TODO: fixme
+    }
+
     public static class Builder {
 
         private boolean dcpEnabled = DCP_ENABLED;
