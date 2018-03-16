b68f601ff1c96a73ac3d98eff06614921d3fb82c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:26:47 +0000

    First commited as before_56631_rev4

--- src/integration/java/com/couchbase/client/core/cluster/DCPMessageTest.java
+++ src/integration/java/com/couchbase/client/core/cluster/DCPMessageTest.java
@@ -73,19 +73,17 @@ public class DCPMessageTest extends ClusterDependentTest {
                 .toList()
                 .toBlocking()
                 .single();
-        assertEquals(1, open.size());
         for (OpenConnectionResponse response : open) {
             assertEquals(ResponseStatus.SUCCESS, response.status());
         }
-
         StreamRequestResponse addStream = cluster()
-                .<StreamRequestResponse>send(new StreamRequestRequest("hello", calculateVBucketForKey("foo"), bucket()))
+                .<StreamRequestResponse>send(new StreamRequestRequest(calculateVBucketForKey("foo"), bucket()))
                 .toBlocking()
                 .single();
         assertEquals(ResponseStatus.SUCCESS, addStream.status());
 
         TestSubscriber<DCPRequest> subscriber = new TestSubscriber<DCPRequest>();
-        open.get(0).connection().subject().takeUntil(Observable.timer(2, TimeUnit.SECONDS)).subscribe((Subscriber) subscriber);
+        addStream.stream().takeUntil(Observable.timer(2, TimeUnit.SECONDS)).subscribe((Subscriber) subscriber);
 
         UpsertResponse foo = cluster()
                 .<UpsertResponse>send(new UpsertRequest("foo", Unpooled.copiedBuffer("bar", CharsetUtil.UTF_8), 1, 0, bucket()))
