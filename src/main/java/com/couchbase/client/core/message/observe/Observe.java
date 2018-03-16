5c479f9dc7f5cfe69351cb4e2f4c3c88e12b181d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:23:46 +0000

    First commited as after_42362_rev5

---
+++ src/main/java/com/couchbase/client/core/message/observe/Observe.java
@@ -0,0 +1,207 @@
+/**
+ * Copyright (C) 2014 Couchbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
+ * IN THE SOFTWARE.
+ */
+
+package com.couchbase.client.core.message.observe;
+
+import com.couchbase.client.core.ClusterFacade;
+import com.couchbase.client.core.config.CouchbaseBucketConfig;
+import com.couchbase.client.core.message.cluster.GetClusterConfigRequest;
+import com.couchbase.client.core.message.cluster.GetClusterConfigResponse;
+import com.couchbase.client.core.message.kv.ObserveRequest;
+import com.couchbase.client.core.message.kv.ObserveResponse;
+import rx.Observable;
+import rx.functions.Func0;
+import rx.functions.Func1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Utility class to handle observe calls and polling logic.
+ *
+ * @author Michael Nitschinger
+ * @since 1.0.1
+ */
+public class Observe {
+
+    /**
+     * Observe disk persistence to the master node of the document only.
+     */
+    public final static short PERSIST_TO_MASTER = -1;
+
+    /**
+     * Do not observe any disk persistence constraint.
+     */
+    public final static short PERSIST_TO_NONE = 0;
+
+    /**
+     * Observe disk persistence of one node (master or replica).
+     */
+    public final static short PERSIST_TO_ONE = 1;
+
+    /**
+     * Observe disk persistence of two nodes (master or replica).
+     */
+    public final static short PERSIST_TO_TWO = 2;
+
+    /**
+     * Observe disk persistence of three nodes (master or replica).
+     */
+    public final static short PERSIST_TO_THREE = 3;
+
+    /**
+     * Observe disk persistence of four nodes (one master and three replicas).
+     */
+    public final static short PERSIST_TO_FOUR = 4;
+
+    /**
+     * Do not observe any replication constraint.
+     */
+    public final static short REPLICATE_TO_NONE = 0;
+
+    /**
+     * Observe replication to one replica.
+     */
+    public final static short REPLICATE_TO_ONE = 1;
+
+    /**
+     * Observe replication to two replicas.
+     */
+    public final static short REPLICATE_TO_TWO = 1;
+
+    /**
+     * Observe replication to three replicas.
+     */
+    public final static short REPLICATE_TO_THREE = 3;
+
+    public static Observable<Boolean> call(final ClusterFacade core, final String bucket, final String id,
+                                           final long cas, final boolean remove, final short persistTo, final short replicateTo) {
+
+        final ObserveResponse.ObserveStatus persistIdentifier;
+        final ObserveResponse.ObserveStatus replicaIdentifier;
+        if (remove) {
+            persistIdentifier = ObserveResponse.ObserveStatus.FOUND_NOT_PERSISTED;
+            replicaIdentifier = ObserveResponse.ObserveStatus.NOT_FOUND_NOT_PERSISTED;
+        } else {
+            persistIdentifier = ObserveResponse.ObserveStatus.FOUND_PERSISTED;
+            replicaIdentifier = ObserveResponse.ObserveStatus.NOT_FOUND_PERSISTED;
+        }
+
+        Observable<ObserveResponse> observeResponses = sendObserveRequests(core, bucket, id, cas, persistTo, replicateTo);
+
+        return observeResponses
+                .toList()
+                .delay(10, TimeUnit.MILLISECONDS)
+                .repeat()
+                .skipWhile(new Func1<List<ObserveResponse>, Boolean>() {
+                    @Override
+                    public Boolean call(List<ObserveResponse> observeResponses) {
+                        int replicated = 0;
+                        int persisted = 0;
+                        boolean persistedMaster = false;
+                        for (ObserveResponse response : observeResponses) {
+                            ObserveResponse.ObserveStatus status = response.observeStatus();
+                            if (response.master()) {
+                                if (status == persistIdentifier) {
+                                    persisted++;
+                                    persistedMaster = true;
+                                }
+                            } else {
+                                if (status == persistIdentifier) {
+                                    persisted++;
+                                    replicated++;
+                                } else if (status == replicaIdentifier) {
+                                    replicated++;
+                                }
+                            }
+                        }
+
+                        boolean persistDone = false;
+                        boolean replicateDone = false;
+
+                        if (persistTo == PERSIST_TO_MASTER && persistedMaster) {
+                            persistDone = true;
+                        } else if (persisted >= persistTo) {
+                            persistDone = true;
+                        }
+
+                        if (replicated >= replicateTo) {
+                            replicateDone = true;
+                        }
+
+                        return !(persistDone && replicateDone);
+                    }
+                })
+                .take(1)
+                .map(new Func1<List<ObserveResponse>, Boolean>() {
+                    @Override
+                    public Boolean call(List<ObserveResponse> observeResponses) {
+                        return true;
+                    }
+                });
+    }
+
+    private static Observable<ObserveResponse> sendObserveRequests(final ClusterFacade core, final String bucket, final String id, final long cas,
+                                                                   final short persistTo, final short replicateTo) {
+        return Observable.defer(new Func0<Observable<ObserveResponse>>() {
+            @Override
+            public Observable<ObserveResponse> call() {
+                return core
+                        .<GetClusterConfigResponse>send(new GetClusterConfigRequest())
+                        .map(new Func1<GetClusterConfigResponse, Integer>() {
+                            @Override
+                            public Integer call(GetClusterConfigResponse response) {
+                                CouchbaseBucketConfig conf = (CouchbaseBucketConfig) response.config().bucketConfig(bucket);
+                                return conf.numberOfReplicas();
+                            }
+                        })
+                        .flatMap(new Func1<Integer, Observable<ObserveResponse>>() {
+                            @Override
+                            public Observable<ObserveResponse> call(Integer replicas) {
+                                List<Observable<ObserveResponse>> obs = new ArrayList<Observable<ObserveResponse>>();
+                                if (persistTo != PERSIST_TO_NONE) {
+                                    obs.add(core.<ObserveResponse>send(new ObserveRequest(id, cas, true, (short) 0, bucket)));
+                                }
+
+                                if (touchesReplica(persistTo) || touchesReplica(replicateTo)) {
+                                    if (replicas >= 1) {
+                                        obs.add(core.<ObserveResponse>send(new ObserveRequest(id, cas, false, (short) 1, bucket)));
+                                    }
+                                    if (replicas >= 2) {
+                                        obs.add(core.<ObserveResponse>send(new ObserveRequest(id, cas, false, (short) 2, bucket)));
+                                    }
+                                    if (replicas == 3) {
+                                        obs.add(core.<ObserveResponse>send(new ObserveRequest(id, cas, false, (short) 3, bucket)));
+                                    }
+                                }
+                                return Observable.merge(obs);
+                            }
+                        });
+            }
+
+            private boolean touchesReplica(short value) {
+                return value > 0;
+            }
+        });
+    }
+}
