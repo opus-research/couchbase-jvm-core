831a3d77193c35cd164f0a61dad3592d321f710d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:31:46 +0000

    First commited as before_85009_rev1

--- src/main/java/com/couchbase/client/core/message/internal/EndpointHealth.java
+++ src/main/java/com/couchbase/client/core/message/internal/EndpointHealth.java
@@ -48,23 +48,14 @@ public class EndpointHealth {
     public EndpointHealth(ServiceType type, LifecycleState state, SocketAddress localAddr, SocketAddress remoteAddr, long lastActivityUs, long pingLatencyUs) {
         this.type = type;
         this.state = state;
-
-        if (localAddr == null) {
-            this.local = null;
-        } else if (!(localAddr instanceof InetSocketAddress)) {
+        if (!(localAddr instanceof InetSocketAddress)) {
             throw new IllegalArgumentException("Right now only InetSocketAddress is supported");
-        } else {
-            this.local = (InetSocketAddress) localAddr;
         }
-
-        if (remoteAddr == null) {
-            this.remote = null;
-        } else if (!(remoteAddr instanceof InetSocketAddress)) {
+        if (!(remoteAddr instanceof InetSocketAddress)) {
             throw new IllegalArgumentException("Right now only InetSocketAddress is supported");
-        } else {
-            this.remote = (InetSocketAddress) remoteAddr;
         }
-
+        this.local = (InetSocketAddress) localAddr;
+        this.remote = (InetSocketAddress) remoteAddr;
         this.lastActivityUs = lastActivityUs;
         this.pingLatencyUs = pingLatencyUs;
     }
@@ -94,11 +85,11 @@ public class EndpointHealth {
     }
 
     public Map<String, Object> toMap() {
-        NetworkAddress ra = remote() == null ? null : NetworkAddress.create(remote().getAddress().getHostAddress());
-        NetworkAddress la = local() == null ? null : NetworkAddress.create(local().getAddress().getHostAddress());
+        NetworkAddress ra = NetworkAddress.create(remote().getAddress().getHostAddress());
+        NetworkAddress la = NetworkAddress.create(local().getAddress().getHostAddress());
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("remote", ra == null ? "" : ra.nameOrAddress() + ":" + remote().getPort());
-        map.put("local", la == null ? "" : la.nameOrAddress() + ":" + local().getPort());
+        map.put("remote", remote() == null ? "" : ra.nameOrAddress() + ":" + remote().getPort());
+        map.put("local", local() == null ? "" : la.nameOrAddress() + ":" + local().getPort());
         map.put("state", state().toString().toLowerCase());
         map.put("last_activity_us", lastActivity());
         map.put("latency_us", pingLatency());
