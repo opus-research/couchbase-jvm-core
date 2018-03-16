7d596de19465b9a8f56116c6ebfcab564a5f150c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:27:01 +0000

    First commited as before_57957_rev1

--- src/main/java/com/couchbase/client/core/dcp/BucketStreamAggregatorState.java
+++ src/main/java/com/couchbase/client/core/dcp/BucketStreamAggregatorState.java
@@ -34,7 +34,7 @@ import java.util.Map;
 
 /**
  * State of the stream aggregator.
- *
+ * <p/>
  * It contains list of the stream states.
  *
  * @author Sergey Avseyev
@@ -43,14 +43,18 @@ import java.util.Map;
 @InterfaceStability.Experimental
 @InterfaceAudience.Public
 public class BucketStreamAggregatorState implements Iterable<BucketStreamState> {
+    private final String name;
     private final Subject<BucketStreamStateUpdatedEvent, BucketStreamStateUpdatedEvent> updates =
             PublishSubject.<BucketStreamStateUpdatedEvent>create().toSerialized();
     private final Map<Short, BucketStreamState> feeds = new HashMap<Short, BucketStreamState>(1024);
 
     /**
      * Creates a new {@link BucketStreamAggregatorState}.
+     *
+     * @param name name of the DCP connection.
      */
-    public BucketStreamAggregatorState() {
+    public BucketStreamAggregatorState(final String name) {
+        this.name = name;
     }
 
     /**
@@ -83,7 +87,7 @@ public class BucketStreamAggregatorState implements Iterable<BucketStreamState> 
     }
 
     /**
-     * Returns state for the partition
+     * Returns state for the vBucket
      *
      * @param partition vBucketID (partition number).
      * @return state or null if state of partition not tracked.
@@ -92,18 +96,21 @@ public class BucketStreamAggregatorState implements Iterable<BucketStreamState> 
         return feeds.get(partition);
     }
 
-    /**
-     * Removes state for the partition.
-     *
-     * @param partition vBucketID (partition number).
-     * @return state or null if state of partition not tracked.
-     */
     public BucketStreamState remove(short partition) {
         return feeds.remove(partition);
     }
 
     public int size() {
         return feeds.size();
+    }
+
+    /**
+     * Returns name of the DCP stream.
+     *
+     * @return name of the DCP stream.
+     */
+    public String name() {
+        return name;
     }
 
     @Override
