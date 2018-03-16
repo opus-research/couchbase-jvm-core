0cf360b960b195ce3d486b8dddd5fe5993db5a8e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:23:43 +0000

    First commited as after_42292_rev1

--- src/main/java/com/couchbase/client/core/message/AbstractCouchbaseRequest.java
+++ src/main/java/com/couchbase/client/core/message/AbstractCouchbaseRequest.java
@@ -94,4 +94,14 @@ public abstract class AbstractCouchbaseRequest implements CouchbaseRequest {
     public String password() {
         return password;
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder(this.getClass().getSimpleName() + "{");
+        sb.append("observable=").append(observable);
+        sb.append(", bucket='").append(bucket).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
