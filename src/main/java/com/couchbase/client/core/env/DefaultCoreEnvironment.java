ec6d58eb598fb6ee30857e35a17acc1af633dfdd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:24:45 +0000

    First commited as after_45990_rev3

--- src/main/java/com/couchbase/client/core/env/DefaultCoreEnvironment.java
+++ src/main/java/com/couchbase/client/core/env/DefaultCoreEnvironment.java
@@ -70,6 +70,7 @@ public class DefaultCoreEnvironment implements CoreEnvironment {
     public static final Delay RETRY_DELAY = Delay.exponential(TimeUnit.MICROSECONDS, 100000, 100);
     public static final RetryStrategy RETRY_STRATEGY = BestEffortRetryStrategy.INSTANCE;
     public static final long MAX_REQUEST_LIFETIME = TimeUnit.SECONDS.toMillis(75);
+    public static final long KEEPALIVEINTERVAL = TimeUnit.SECONDS.toMillis(30);
 
     public static String PACKAGE_NAME_AND_VERSION = "couchbase-jvm-core";
     public static String USER_AGENT = PACKAGE_NAME_AND_VERSION;
@@ -143,6 +144,7 @@ public class DefaultCoreEnvironment implements CoreEnvironment {
     private final String packageNameAndVersion;
     private final RetryStrategy retryStrategy;
     private final long maxRequestLifetime;
+    private final long keepAliveInterval;
 
     private static final int MAX_ALLOWED_INSTANCES = 1;
     private static volatile int instanceCounter = 0;
@@ -182,6 +184,7 @@ public class DefaultCoreEnvironment implements CoreEnvironment {
         retryDelay = builder.retryDelay();
         retryStrategy = builder.retryStrategy();
         maxRequestLifetime = longPropertyOr("maxRequestLifetime", builder.maxRequestLifetime());
+        keepAliveInterval = longPropertyOr("keepAliveInterval", builder.keepAliveInterval());
 
         this.ioPool = builder.ioPool() == null
             ? new NioEventLoopGroup(ioPoolSize(), new DefaultThreadFactory("cb-io", true)) : builder.ioPool();
@@ -399,6 +402,11 @@ public class DefaultCoreEnvironment implements CoreEnvironment {
         return maxRequestLifetime;
     }
 
+    @Override
+    public long keepAliveInterval() {
+        return this.keepAliveInterval;
+    }
+
     public static class Builder implements CoreEnvironment {
 
         private boolean dcpEnabled = DCP_ENABLED;
@@ -429,6 +437,7 @@ public class DefaultCoreEnvironment implements CoreEnvironment {
         private EventLoopGroup ioPool;
         private Scheduler scheduler;
         private long maxRequestLifetime = MAX_REQUEST_LIFETIME;
+        private long keepAliveInterval = KEEPALIVEINTERVAL;
 
         protected Builder() {
         }
@@ -718,6 +727,16 @@ public class DefaultCoreEnvironment implements CoreEnvironment {
             return this;
         }
 
+        @Override
+        public long keepAliveInterval() {
+            return keepAliveInterval;
+        }
+
+        public Builder keepAliveInterval(long keepAliveIntervalMilliseconds) {
+            this.keepAliveInterval = keepAliveIntervalMilliseconds;
+            return this;
+        }
+
         public DefaultCoreEnvironment build() {
             return new DefaultCoreEnvironment(this);
         }
@@ -753,6 +772,7 @@ public class DefaultCoreEnvironment implements CoreEnvironment {
         sb.append(", retryDelay=").append(retryDelay);
         sb.append(", reconnectDelay=").append(reconnectDelay);
         sb.append(", observeIntervalDelay=").append(observeIntervalDelay);
+        sb.append(", keepAliveInterval=").append(keepAliveInterval);
         sb.append('}');
         return sb.toString();
     }
