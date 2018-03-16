9fdcd84e3cfe9e17900906cb6b3dfc095579dc60
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:29:28 +0000

    First commited as after_72365_rev8

---
+++ src/main/java/com/couchbase/client/core/env/QueryServiceConfig.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.couchbase.client.core.env;
+
+public final class QueryServiceConfig extends AbstractServiceConfig {
+
+    private QueryServiceConfig(int minEndpoints, int maxEndpoints) {
+        super(minEndpoints, maxEndpoints, false, 0);
+    }
+
+    public static QueryServiceConfig create(int minEndpoints, int maxEndpoints) {
+        return new QueryServiceConfig(minEndpoints, maxEndpoints);
+    }
+
+    @Override
+    public String toString() {
+        return "QueryServiceConfig{" +
+                "minEndpoints=" + minEndpoints() +
+                ", maxEndpoints=" + maxEndpoints() +
+                ", pipelined=" + isPipelined() +
+                ", idleTime=" + idleTime() +
+                '}';
+    }
+
+}
