9ebb82f92eaa60d455e0b50f4abc7e3d3c86542c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:30:27 +0000

    First commited as before_77515_rev6

--- src/main/java/com/couchbase/client/core/retry/RetryHelper.java
+++ src/main/java/com/couchbase/client/core/retry/RetryHelper.java
@@ -41,10 +41,6 @@ public class RetryHelper {
      */
     public static void retryOrCancel(final CoreEnvironment environment, final CouchbaseRequest request,
         final EventSink<ResponseEvent> responseBuffer) {
-        if (!request.isActive()) {
-            return;
-        }
-
         if (environment.retryStrategy().shouldRetry(request, environment)) {
             retry(request, responseBuffer);
         } else {
