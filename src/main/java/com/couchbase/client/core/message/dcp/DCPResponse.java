175e882bd83e96d71207b1e0e6f3f7e1d79ae793
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:23:56 +0000

    First commited as after_43782_rev6

---
+++ src/main/java/com/couchbase/client/core/message/dcp/DCPResponse.java
@@ -0,0 +1,10 @@
+package com.couchbase.client.core.message.dcp;
+
+import com.couchbase.client.core.message.CouchbaseResponse;
+
+/**
+ * @author Sergey Avseyev
+ * @since 1.0.2
+ */
+public interface DCPResponse extends CouchbaseResponse {
+}
