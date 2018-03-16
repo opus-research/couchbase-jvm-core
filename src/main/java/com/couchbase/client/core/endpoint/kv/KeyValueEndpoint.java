0c182b2143cddb2dc67999055c9236b24768beb1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:29:47 +0000

    First commited as before_74846_rev2

--- src/main/java/com/couchbase/client/core/endpoint/kv/KeyValueEndpoint.java
+++ src/main/java/com/couchbase/client/core/endpoint/kv/KeyValueEndpoint.java
@@ -55,8 +55,8 @@ public class KeyValueEndpoint extends AbstractEndpoint {
         pipeline
             .addLast(new BinaryMemcacheClientCodec())
             .addLast(new BinaryMemcacheObjectAggregator(Integer.MAX_VALUE))
-            .addLast(new KeyValueFeatureHandler(environment()))
             .addLast(new KeyValueAuthHandler(bucket(), password()))
+            .addLast(new KeyValueFeatureHandler(environment()))
             .addLast(new KeyValueHandler(this, responseBuffer(), false, true));
     }
 
