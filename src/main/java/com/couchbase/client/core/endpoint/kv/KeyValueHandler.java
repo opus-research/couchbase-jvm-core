7321fe6651ac97e2e50101aacf0cf4fb7efee049
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:24:19 +0000

    First commited as after_44781_rev1

--- src/main/java/com/couchbase/client/core/endpoint/kv/KeyValueHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/kv/KeyValueHandler.java
@@ -162,12 +162,6 @@ public class KeyValueHandler
             request.getExtras().retain();
         }
 
-        if (request instanceof FullBinaryMemcacheRequest) {
-            ByteBuf content = ((FullBinaryMemcacheRequest) request).content();
-            if (content != null) {
-                content.retain();
-            }
-        }
         return request;
     }
 
