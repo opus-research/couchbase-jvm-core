4cfad560012f6499d0eeebb20c9c20666b4e562d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:25:03 +0000

    First commited as before_47859_rev1

--- src/main/java/com/couchbase/client/core/message/view/GetDesignDocumentResponse.java
+++ src/main/java/com/couchbase/client/core/message/view/GetDesignDocumentResponse.java
@@ -25,8 +25,9 @@ import com.couchbase.client.core.message.AbstractCouchbaseResponse;
 import com.couchbase.client.core.message.CouchbaseRequest;
 import com.couchbase.client.core.message.ResponseStatus;
 import io.netty.buffer.ByteBuf;
+import io.netty.util.ReferenceCounted;
 
-public class GetDesignDocumentResponse extends AbstractCouchbaseResponse {
+public class GetDesignDocumentResponse extends AbstractCouchbaseResponse implements ReferenceCounted {
 
     private final String name;
     private final boolean development;
@@ -49,5 +50,32 @@ public class GetDesignDocumentResponse extends AbstractCouchbaseResponse {
 
     public ByteBuf content() {
         return content;
+    }
+
+    @Override
+    public int refCnt() {
+        return content.refCnt();
+    }
+
+    @Override
+    public GetDesignDocumentResponse retain() {
+        content.retain();
+        return this;
+    }
+
+    @Override
+    public GetDesignDocumentResponse retain(int increment) {
+        content.retain(increment);
+        return this;
+    }
+
+    @Override
+    public boolean release() {
+        return content.release();
+    }
+
+    @Override
+    public boolean release(int decrement) {
+        return content.release(decrement);
     }
 }
