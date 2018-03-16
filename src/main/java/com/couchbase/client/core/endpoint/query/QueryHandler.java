faf926eaeeddec71c1264d86362fad8868b401e2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:24:34 +0000

    First commited as before_45436_rev1

--- src/main/java/com/couchbase/client/core/endpoint/query/QueryHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/query/QueryHandler.java
@@ -372,17 +372,17 @@ public class QueryHandler extends AbstractGenericHandler<HttpObject, HttpRequest
         int endNextToken = bytesBeforeInResponse(':');
         ByteBuf peekSlice = responseContent.readSlice(endNextToken + 1);
         String peek = peekSlice.toString(CHARSET);
-        if (peek.contains("\"signature\":")) {
+        if (peek.contains("\"signature\"")) {
             return QUERY_STATE_SIGNATURE;
-        } else if (peek.endsWith("\"results\":")) {
+        } else if (peek.contains("\"results\"")) {
             return QUERY_STATE_ROWS;
-        } else if (peek.endsWith("\"status\":")) {
+        } else if (peek.contains("\"status\"")) {
             return QUERY_STATE_STATUS;
-        } else if (peek.endsWith("\"errors\":")) {
+        } else if (peek.contains("\"errors\"")) {
             return QUERY_STATE_ERROR;
-        } else if (peek.endsWith("\"warnings\":")) {
+        } else if (peek.contains("\"warnings\"")) {
             return QUERY_STATE_WARNING;
-        } else if (peek.endsWith("\"metrics\":")) {
+        } else if (peek.contains("\"metrics\"")) {
             return QUERY_STATE_INFO;
         } else {
             IllegalStateException e = new IllegalStateException("Error parsing query response (in TRANSITION) at " + peek);
@@ -397,16 +397,13 @@ public class QueryHandler extends AbstractGenericHandler<HttpObject, HttpRequest
      * For now skip the signature.
      */
     private void skipQuerySignature() {
-        int nextColon = bytesBeforeInResponse(':');
+        //TODO ultimately send the signature back to the client
         int openPos = bytesBeforeInResponse('{');
-        if (openPos < nextColon) { //checks for empty signature
-            int closePos = findSectionClosingPosition(responseContent, '{', '}');
-            if (closePos > 0) {
-                int length = closePos - openPos - responseContent.readerIndex() + 1;
-                responseContent.skipBytes(openPos);
-                //TODO ultimately send the signature back to the client
-                ByteBuf signature = responseContent.readSlice(length);
-            }
+        int closePos = findSectionClosingPosition(responseContent, '{', '}');
+        if (closePos > 0) {
+            int length = closePos - openPos - responseContent.readerIndex() + 1;
+            responseContent.skipBytes(openPos);
+            ByteBuf signature = responseContent.readSlice(length);
         }
         queryParsingState = transitionToNextToken();
     }
