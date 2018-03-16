1ab0052035013781f93ca0b967dac2169c8a7369
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:29:07 +0000

    First commited as after_70577_rev1

--- src/main/java/com/couchbase/client/core/RequestCancelledException.java
+++ src/main/java/com/couchbase/client/core/RequestCancelledException.java
@@ -15,6 +15,21 @@
  */
 package com.couchbase.client.core;
 
+/**
+ * This exception is thrown on two events
+ *
+ *  - The request is dispatched and the underlying connection to a particular endpoint on the server is closed.
+ *  So the outstanding requests in flight are cancelled out. It is not definitive if the operation indeed succeeded
+ *  on the server and the socket error happened while a response was on its way out. On this event, it should be worth
+ *  to retry idempotent requests directly and have a special retry handler for the non-idempotent ones.
+ *
+ *  - The request cannot be dispatched to the server's endpoint due to various reasons like downed node, request queue limit,
+ *  particular service failure on the node, ... If the failure was due to request queue limit, try increasing the value on the system
+ *  property "com.couchbase.sentRequestQueueLimit". On this event, any request can be retried irrespective of idempotency.
+ *
+ * @author Michael Nitschinger
+ * @author Subhashni Balakrishnan
+ */
 public class RequestCancelledException extends CouchbaseException {
 
     public RequestCancelledException() {
