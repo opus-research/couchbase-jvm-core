34944755e1ffbddbbca4af39f9d20846aa4f9ff8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:28:29 +0000

    First commited as before_64018_rev4

--- src/test/java/com/couchbase/client/core/node/locate/KeyValueLocatorTest.java
+++ src/test/java/com/couchbase/client/core/node/locate/KeyValueLocatorTest.java
@@ -1,23 +1,17 @@
-/**
- * Copyright (C) 2014 Couchbase, Inc.
+/*
+ * Copyright (c) 2016 Couchbase, Inc.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
- * IN THE SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.couchbase.client.core.node.locate;
 
@@ -56,6 +50,7 @@ public class KeyValueLocatorTest {
     public void shouldLocateGetRequestForCouchbaseBucket() throws Exception {
         Locator locator = new KeyValueLocator();
 
+
         NodeInfo nodeInfo1 = new DefaultNodeInfo("foo", "192.168.56.101:11210", Collections.EMPTY_MAP);
         NodeInfo nodeInfo2 = new DefaultNodeInfo("foo", "192.168.56.102:11210", Collections.EMPTY_MAP);
 
@@ -74,7 +69,7 @@ public class KeyValueLocatorTest {
         when(configMock.bucketConfig("bucket")).thenReturn(bucketMock);
         when(bucketMock.nodes()).thenReturn(Arrays.asList(nodeInfo1, nodeInfo2));
         when(bucketMock.numberOfPartitions()).thenReturn(1024);
-        when(bucketMock.nodeIndexForMaster(656, false)).thenReturn((short) 0);
+        when(bucketMock.nodeIndexForMaster(656)).thenReturn((short) 0);
         when(bucketMock.nodeAtIndex(0)).thenReturn(nodeInfo1);
 
         locator.locateAndDispatch(getRequestMock, nodes, configMock, null, null);
@@ -101,112 +96,6 @@ public class KeyValueLocatorTest {
         locator.locateAndDispatch(requestMock, nodes, mock(ClusterConfig.class), null, null);
         verify(node1Mock, never()).send(requestMock);
         verify(node2Mock, times(1)).send(requestMock);
-    }
-
-    @Test
-    public void shouldPickFastForwardIfAvailableAndRetry() throws Exception {
-        Locator locator = new KeyValueLocator();
-
-        // Setup 2 nodes
-        NodeInfo nodeInfo1 = new DefaultNodeInfo("foo", "192.168.56.101:11210", Collections.EMPTY_MAP);
-        NodeInfo nodeInfo2 = new DefaultNodeInfo("foo", "192.168.56.102:11210", Collections.EMPTY_MAP);
-        List<Node> nodes = new ArrayList<Node>();
-        Node node1Mock = mock(Node.class);
-        when(node1Mock.hostname()).thenReturn(InetAddress.getByName("192.168.56.101"));
-        Node node2Mock = mock(Node.class);
-        when(node2Mock.hostname()).thenReturn(InetAddress.getByName("192.168.56.102"));
-        nodes.addAll(Arrays.asList(node1Mock, node2Mock));
-
-        // Configure Cluster and Bucket config
-        ClusterConfig configMock = mock(ClusterConfig.class);
-        CouchbaseBucketConfig bucketMock = mock(CouchbaseBucketConfig.class);
-        when(configMock.bucketConfig("bucket")).thenReturn(bucketMock);
-        when(bucketMock.nodes()).thenReturn(Arrays.asList(nodeInfo1, nodeInfo2));
-        when(bucketMock.numberOfPartitions()).thenReturn(1024);
-        when(bucketMock.nodeAtIndex(0)).thenReturn(nodeInfo1);
-        when(bucketMock.nodeAtIndex(1)).thenReturn(nodeInfo2);
-        when(bucketMock.hasFastForwardMap()).thenReturn(true);
-
-        // Fake a vbucket move in ffwd map from node 0 to node 1
-        when(bucketMock.nodeIndexForMaster(656, false)).thenReturn((short) 0);
-        when(bucketMock.nodeIndexForMaster(656, true)).thenReturn((short) 1);
-
-        // Create Request
-        GetRequest getRequestMock = mock(GetRequest.class);
-        when(getRequestMock.bucket()).thenReturn("bucket");
-        when(getRequestMock.key()).thenReturn("key");
-        when(getRequestMock.keyBytes()).thenReturn("key".getBytes(CharsetUtil.UTF_8));
-
-        // Dispatch with retry 0
-        when(getRequestMock.retryCount()).thenReturn(0);
-        locator.locateAndDispatch(getRequestMock, nodes, configMock, null, null);
-        verify(node1Mock, times(1)).send(getRequestMock);
-        verify(node2Mock, never()).send(getRequestMock);
-
-        // Dispatch with retry 1
-        when(getRequestMock.retryCount()).thenReturn(1);
-        locator.locateAndDispatch(getRequestMock, nodes, configMock, null, null);
-        verify(node1Mock, times(1)).send(getRequestMock);
-        verify(node2Mock, times(1)).send(getRequestMock);
-
-        // Dispatch with retry 5
-        when(getRequestMock.retryCount()).thenReturn(5);
-        locator.locateAndDispatch(getRequestMock, nodes, configMock, null, null);
-        verify(node1Mock, times(1)).send(getRequestMock);
-        verify(node2Mock, times(2)).send(getRequestMock);
-    }
-
-    @Test
-    public void shouldPickCurrentIfNoFFMapAndRetry() throws Exception {
-        Locator locator = new KeyValueLocator();
-
-        // Setup 2 nodes
-        NodeInfo nodeInfo1 = new DefaultNodeInfo("foo", "192.168.56.101:11210", Collections.EMPTY_MAP);
-        NodeInfo nodeInfo2 = new DefaultNodeInfo("foo", "192.168.56.102:11210", Collections.EMPTY_MAP);
-        List<Node> nodes = new ArrayList<Node>();
-        Node node1Mock = mock(Node.class);
-        when(node1Mock.hostname()).thenReturn(InetAddress.getByName("192.168.56.101"));
-        Node node2Mock = mock(Node.class);
-        when(node2Mock.hostname()).thenReturn(InetAddress.getByName("192.168.56.102"));
-        nodes.addAll(Arrays.asList(node1Mock, node2Mock));
-
-        // Configure Cluster and Bucket config
-        ClusterConfig configMock = mock(ClusterConfig.class);
-        CouchbaseBucketConfig bucketMock = mock(CouchbaseBucketConfig.class);
-        when(configMock.bucketConfig("bucket")).thenReturn(bucketMock);
-        when(bucketMock.nodes()).thenReturn(Arrays.asList(nodeInfo1, nodeInfo2));
-        when(bucketMock.numberOfPartitions()).thenReturn(1024);
-        when(bucketMock.nodeAtIndex(0)).thenReturn(nodeInfo1);
-        when(bucketMock.nodeAtIndex(1)).thenReturn(nodeInfo2);
-        when(bucketMock.hasFastForwardMap()).thenReturn(false);
-
-        // Fake a vbucket move in ffwd map from node 0 to node 1
-        when(bucketMock.nodeIndexForMaster(656, false)).thenReturn((short) 0);
-        when(bucketMock.nodeIndexForMaster(656, true)).thenReturn((short) 1);
-
-        // Create Request
-        GetRequest getRequestMock = mock(GetRequest.class);
-        when(getRequestMock.bucket()).thenReturn("bucket");
-        when(getRequestMock.key()).thenReturn("key");
-        when(getRequestMock.keyBytes()).thenReturn("key".getBytes(CharsetUtil.UTF_8));
-
-        // Dispatch with retry 0
-        when(getRequestMock.retryCount()).thenReturn(0);
-        locator.locateAndDispatch(getRequestMock, nodes, configMock, null, null);
-        verify(node1Mock, times(1)).send(getRequestMock);
-        verify(node2Mock, never()).send(getRequestMock);
-
-        // Dispatch with retry 1
-        when(getRequestMock.retryCount()).thenReturn(1);
-        locator.locateAndDispatch(getRequestMock, nodes, configMock, null, null);
-        verify(node1Mock, times(2)).send(getRequestMock);
-        verify(node2Mock, never()).send(getRequestMock);
-
-        // Dispatch with retry 5
-        when(getRequestMock.retryCount()).thenReturn(5);
-        locator.locateAndDispatch(getRequestMock, nodes, configMock, null, null);
-        verify(node1Mock, times(3)).send(getRequestMock);
-        verify(node2Mock, never()).send(getRequestMock);
     }
 
 }
