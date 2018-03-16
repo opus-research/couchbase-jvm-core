4c24d675821ae43d1abe82f80088e227a62661ba
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:30:07 +0000

    First commited as after_75265_rev1

--- src/main/java/com/couchbase/client/core/endpoint/config/ConfigHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/config/ConfigHandler.java
@@ -36,14 +36,20 @@ import com.couchbase.client.core.message.config.FlushRequest;
 import com.couchbase.client.core.message.config.FlushResponse;
 import com.couchbase.client.core.message.config.GetDesignDocumentsRequest;
 import com.couchbase.client.core.message.config.GetDesignDocumentsResponse;
+import com.couchbase.client.core.message.config.GetUsersRequest;
+import com.couchbase.client.core.message.config.GetUsersResponse;
 import com.couchbase.client.core.message.config.InsertBucketRequest;
 import com.couchbase.client.core.message.config.InsertBucketResponse;
 import com.couchbase.client.core.message.config.RemoveBucketRequest;
 import com.couchbase.client.core.message.config.RemoveBucketResponse;
+import com.couchbase.client.core.message.config.RemoveUserRequest;
+import com.couchbase.client.core.message.config.RemoveUserResponse;
 import com.couchbase.client.core.message.config.RestApiRequest;
 import com.couchbase.client.core.message.config.RestApiResponse;
 import com.couchbase.client.core.message.config.UpdateBucketRequest;
 import com.couchbase.client.core.message.config.UpdateBucketResponse;
+import com.couchbase.client.core.message.config.UpsertUserRequest;
+import com.couchbase.client.core.message.config.UpsertUserResponse;
 import com.couchbase.client.core.service.ServiceType;
 import com.lmax.disruptor.EventSink;
 import com.lmax.disruptor.RingBuffer;
@@ -130,9 +136,10 @@ public class ConfigHandler extends AbstractGenericHandler<HttpObject, HttpReques
             return encodeRestApiRequest(ctx, (RestApiRequest) msg);
         }
         HttpMethod httpMethod = HttpMethod.GET;
-        if (msg instanceof FlushRequest || msg instanceof InsertBucketRequest || msg instanceof UpdateBucketRequest) {
+        if (msg instanceof FlushRequest || msg instanceof InsertBucketRequest
+                || msg instanceof UpdateBucketRequest || msg instanceof UpsertUserRequest) {
             httpMethod = HttpMethod.POST;
-        } else if (msg instanceof RemoveBucketRequest) {
+        } else if (msg instanceof RemoveBucketRequest || msg instanceof RemoveUserRequest) {
             httpMethod = HttpMethod.DELETE;
         }
 
@@ -141,6 +148,8 @@ public class ConfigHandler extends AbstractGenericHandler<HttpObject, HttpReques
             content = Unpooled.copiedBuffer(((InsertBucketRequest) msg).payload(), CharsetUtil.UTF_8);
         } else if (msg instanceof UpdateBucketRequest) {
             content = Unpooled.copiedBuffer(((UpdateBucketRequest) msg).payload(), CharsetUtil.UTF_8);
+        } else if (msg instanceof UpsertUserRequest) {
+            content = Unpooled.copiedBuffer(((UpsertUserRequest) msg).payload(), CharsetUtil.UTF_8);
         } else {
             content = Unpooled.EMPTY_BUFFER;
         }
@@ -236,6 +245,12 @@ public class ConfigHandler extends AbstractGenericHandler<HttpObject, HttpReques
             } else if (request instanceof FlushRequest) {
                 boolean done = responseHeader.getStatus().code() != 201;
                 response = new FlushResponse(done, body, status);
+            } else if (request instanceof GetUsersRequest) {
+                response = new GetUsersResponse(body, status, request);
+            } else if (request instanceof UpsertUserRequest) {
+                response = new UpsertUserResponse(body, status);
+            }  else if (request instanceof RemoveUserRequest) {
+                response = new RemoveUserResponse(status);
             } else if (request instanceof RestApiRequest) {
                 response = new RestApiResponse((RestApiRequest) request, responseHeader.getStatus(),
                         responseHeader.headers(), body);
