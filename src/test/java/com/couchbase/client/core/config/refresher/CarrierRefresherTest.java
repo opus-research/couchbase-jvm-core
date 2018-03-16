9125cc20510fc1c130a72cd982a13226902fa0bb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:30:22 +0000

    First commited as after_76934_rev1

--- src/test/java/com/couchbase/client/core/config/refresher/CarrierRefresherTest.java
+++ src/test/java/com/couchbase/client/core/config/refresher/CarrierRefresherTest.java
@@ -40,6 +40,7 @@ import rx.Observable;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -410,5 +411,49 @@ public class CarrierRefresherTest {
         }
         // Make sure we got more calls over the 10ms period than below.
         assertTrue(good > bad);
+    }
+
+    @Test
+    public void shouldShiftNodeList() {
+        ClusterFacade cluster = mock(ClusterFacade.class);
+        CarrierRefresher refresher = new CarrierRefresher(ENVIRONMENT, cluster);
+
+        List<Integer> list = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4));
+        refresher.shiftNodeList(list);
+        assertEquals(Arrays.asList(0, 1, 2, 3, 4), list); // shift by 0
+
+        list = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4));
+        refresher.shiftNodeList(list);
+        assertEquals(Arrays.asList(1, 2, 3, 4, 0), list); // shift by 1
+
+        list = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4));
+        refresher.shiftNodeList(list);
+        assertEquals(Arrays.asList(2, 3, 4, 0, 1), list); // shift by 2
+
+        list = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4));
+        refresher.shiftNodeList(list);
+        assertEquals(Arrays.asList(3, 4, 0, 1, 2), list); // shift by 3
+
+        list = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4));
+        refresher.shiftNodeList(list);
+        assertEquals(Arrays.asList(4, 0, 1, 2, 3), list); // shift by 4
+
+        list = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4));
+        refresher.shiftNodeList(list);
+        assertEquals(Arrays.asList(0, 1, 2, 3, 4), list); // shift by 0
+
+        list = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4));
+        refresher.shiftNodeList(list);
+        assertEquals(Arrays.asList(1, 2, 3, 4, 0), list); // shift by 1
+    }
+
+    @Test
+    public void shouldUseShiftedNodeListOnTaintedPolling() {
+
+    }
+
+    @Test
+    public void shouldUseShiftedNodeListOnRefreshPolling() {
+
     }
 }
