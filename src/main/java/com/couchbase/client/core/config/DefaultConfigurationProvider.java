2651a64b9adbc07a2ad6c31a1adb4f6a1893c398
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:26:51 +0000

    First commited as before_56694_rev2

--- src/main/java/com/couchbase/client/core/config/DefaultConfigurationProvider.java
+++ src/main/java/com/couchbase/client/core/config/DefaultConfigurationProvider.java
@@ -333,11 +333,6 @@ public class DefaultConfigurationProvider implements ConfigurationProvider {
     public void signalOutdated() {
         LOGGER.debug("Received signal for outdated configuration.");
 
-        if (currentConfig.get().bucketConfigs().isEmpty()) {
-            LOGGER.debug("Ignoring outdated signal, since no buckets are open.");
-            return;
-        }
-
         for (Refresher refresher : refreshers.values()) {
             refresher.refresh(currentConfig.get());
         }
