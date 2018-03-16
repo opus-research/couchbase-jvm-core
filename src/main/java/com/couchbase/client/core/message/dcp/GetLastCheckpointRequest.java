1fc3aaa170928631540741219eaa845105cf3496
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:27:31 +0000

    First commited as before_60928_rev1

--- src/main/java/com/couchbase/client/core/message/dcp/GetLastCheckpointRequest.java
+++ src/main/java/com/couchbase/client/core/message/dcp/GetLastCheckpointRequest.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (C) 2014 Couchbase, Inc.
+/*
+ * Copyright (c) 2016 Couchbase, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,15 +19,24 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
  * IN THE SOFTWARE.
  */
-package com.couchbase.client.core.node.locate;
 
-import com.couchbase.client.core.config.ClusterConfig;
-import com.couchbase.client.core.message.CouchbaseRequest;
-import com.couchbase.client.core.node.Node;
+package com.couchbase.client.core.message.dcp;
 
-import java.util.List;
+/**
+ * Returns the last checkpoint for a particular VBucket. This is useful
+ * for starting a DCP stream from wherever the server currently is.
+ *
+ * @author Sergey Avseyev
+ * @since 1.2.6
+ */
+public class GetLastCheckpointRequest extends AbstractDCPRequest {
+    public GetLastCheckpointRequest(final short partition, final String bucket) {
+        super(bucket, null);
+        partition(partition);
+    }
 
-public interface Locator {
-
-    Node[] locate(CouchbaseRequest request, List<Node> nodes, ClusterConfig config);
+    public GetLastCheckpointRequest(final short partition, final String bucket, final String password) {
+        super(bucket, password);
+        partition(partition);
+    }
 }
--- src/main/java/com/couchbase/client/core/message/dcp/GetLastCheckpointResponse.java
+++ src/main/java/com/couchbase/client/core/message/dcp/GetLastCheckpointResponse.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (C) 2014 Couchbase, Inc.
+/*
+ * Copyright (c) 2016 Couchbase, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,15 +19,38 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
  * IN THE SOFTWARE.
  */
-package com.couchbase.client.core.node.locate;
 
-import com.couchbase.client.core.config.ClusterConfig;
+package com.couchbase.client.core.message.dcp;
+
 import com.couchbase.client.core.message.CouchbaseRequest;
-import com.couchbase.client.core.node.Node;
+import com.couchbase.client.core.message.ResponseStatus;
 
-import java.util.List;
+/**
+ * @author Sergey Avseyev
+ * @since 1.2.6
+ */
+public class GetLastCheckpointResponse extends AbstractDCPResponse {
+    private final long sequenceNumber;
 
-public interface Locator {
+    /**
+     * Creates {@link GetLastCheckpointResponse}.
+     *
+     * @param status         the status of the response.
+     * @param sequenceNumber the most recent sequence number.
+     * @param request
+     */
+    public GetLastCheckpointResponse(final ResponseStatus status, final long sequenceNumber,
+                                     final CouchbaseRequest request) {
+        super(status, request);
+        this.sequenceNumber = sequenceNumber;
+    }
 
-    Node[] locate(CouchbaseRequest request, List<Node> nodes, ClusterConfig config);
+    /**
+     * Each mutation that occurs on a vBucket is assigned a number, which strictly
+     * increases as events are assigned sequence numbers, that can be used to order
+     * that event against other mutations within the same vBucket.
+     */
+    public long sequenceNumber() {
+        return sequenceNumber;
+    }
 }
