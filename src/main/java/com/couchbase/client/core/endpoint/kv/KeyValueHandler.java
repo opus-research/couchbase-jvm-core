19c133e95271ca222e3ff95b90f7226a61c0297a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:29:38 +0000

    First commited as after_73872_rev1

--- src/main/java/com/couchbase/client/core/endpoint/kv/KeyValueHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/kv/KeyValueHandler.java
@@ -348,7 +348,7 @@ public class KeyValueHandler
         byte[] key = msg.keyBytes();
         short keyLength = (short) key.length;
         byte extrasLength = (byte) extras.readableBytes();
-        FullBinaryMemcacheRequest request = new DefaultFullBinaryMemcacheRequest(key, extras, msg.content());
+        FullBinaryMemcacheRequest request = new DefaultFullBinaryMemcacheRequest(key, extras, msg.content().copy());
 
         if (msg instanceof InsertRequest) {
             request.setOpcode(OP_INSERT);
