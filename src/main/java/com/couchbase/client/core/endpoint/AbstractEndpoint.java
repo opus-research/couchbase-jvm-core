9bebc1807acab1f9303706f8ce2d85725f8171d8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:31:33 +0000

    First commited as before_83879_rev1

--- src/main/java/com/couchbase/client/core/endpoint/AbstractEndpoint.java
+++ src/main/java/com/couchbase/client/core/endpoint/AbstractEndpoint.java
@@ -338,21 +338,7 @@ public abstract class AbstractEndpoint extends AbstractStateMachine<LifecycleSta
                 bootstrap.connect().addListener(new ChannelFutureListener() {
                     @Override
                     public void operationComplete(ChannelFuture cf) throws Exception {
-                        if (ss.isUnsubscribed()) {
-                            if (cf.isSuccess() && cf.channel() != null) {
-                                cf.channel().close().addListener(new ChannelFutureListener() {
-                                    @Override
-                                    public void operationComplete(ChannelFuture future) throws Exception {
-                                        if (!future.isSuccess()) {
-                                            LOGGER.debug("Got exception while disconnecting " +
-                                                "stray connect attempt.", future.cause());
-                                        }
-                                    }
-                                });
-                            }
-                        } else {
-                            ss.onSuccess(cf);
-                        }
+                        ss.onSuccess(cf);
                     }
                 });
             }
@@ -377,7 +363,7 @@ public abstract class AbstractEndpoint extends AbstractStateMachine<LifecycleSta
         .subscribe(new SingleSubscriber<ChannelFuture>() {
             @Override
             public void onSuccess(ChannelFuture future) {
-                if (disconnected) {
+                if (state() == LifecycleState.DISCONNECTING || state() == LifecycleState.DISCONNECTED) {
                     LOGGER.debug(logIdent(channel, AbstractEndpoint.this) + "Endpoint connect completed, "
                             + "but got instructed to disconnect in the meantime.");
                     transitionState(LifecycleState.DISCONNECTED);
@@ -569,7 +555,7 @@ public abstract class AbstractEndpoint extends AbstractStateMachine<LifecycleSta
             signalConfigReload();
         }
 
-        if (state() == LifecycleState.CONNECTED) {
+        if (state() == LifecycleState.CONNECTED || state() == LifecycleState.CONNECTING) {
             transitionState(LifecycleState.DISCONNECTED);
             connect(false).subscribe(new Subscriber<LifecycleState>() {
                 @Override
