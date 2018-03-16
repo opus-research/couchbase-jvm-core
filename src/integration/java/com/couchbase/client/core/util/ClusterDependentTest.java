ba9dbf337786e2c1e6cd6fae0e98edf71d02b857
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:30:58 +0000

    First commited as before_80511_rev2

--- src/integration/java/com/couchbase/client/core/util/ClusterDependentTest.java
+++ src/integration/java/com/couchbase/client/core/util/ClusterDependentTest.java
@@ -66,7 +66,7 @@ public class ClusterDependentTest {
 
     static {
         ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
-        System.setProperty("com.couchbase.xerrorEnabled", "false");
+        System.setProperty("com.couchbase.xerrorEnabled", "true");
     }
 
     private static final String seedNode = TestProperties.seedNode();
