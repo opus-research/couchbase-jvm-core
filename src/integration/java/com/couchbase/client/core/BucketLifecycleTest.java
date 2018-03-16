099cfe7f78efc57eca968d99dc50ed9921780cfb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:27:22 +0000

    First commited as before_60653_rev1

--- src/integration/java/com/couchbase/client/core/BucketLifecycleTest.java
+++ src/integration/java/com/couchbase/client/core/BucketLifecycleTest.java
@@ -21,30 +21,16 @@
  */
 package com.couchbase.client.core;
 
-import com.couchbase.client.core.config.BucketType;
 import com.couchbase.client.core.config.ConfigurationException;
 import com.couchbase.client.core.message.ResponseStatus;
-import com.couchbase.client.core.message.cluster.CloseBucketRequest;
-import com.couchbase.client.core.message.cluster.DisconnectRequest;
-import com.couchbase.client.core.message.cluster.GetClusterConfigRequest;
 import com.couchbase.client.core.message.cluster.OpenBucketRequest;
 import com.couchbase.client.core.message.cluster.OpenBucketResponse;
 import com.couchbase.client.core.message.cluster.SeedNodesRequest;
-import com.couchbase.client.core.message.config.InsertBucketRequest;
-import com.couchbase.client.core.message.config.RemoveBucketRequest;
-import com.couchbase.client.core.message.internal.AddNodeRequest;
-import com.couchbase.client.core.message.internal.AddNodeResponse;
-import com.couchbase.client.core.message.internal.AddServiceRequest;
-import com.couchbase.client.core.message.internal.AddServiceResponse;
-import com.couchbase.client.core.service.ServiceType;
 import com.couchbase.client.core.util.TestProperties;
 import org.junit.Test;
 import rx.Observable;
 import rx.functions.Func0;
-import rx.functions.Func1;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
@@ -132,51 +118,6 @@ public class BucketLifecycleTest {
             .single();
 
         assertEquals(ResponseStatus.SUCCESS, response.status());
-    }
-
-    @Test
-    public void shouldRemoveBucketWithoutCausingAuthErrors() throws InterruptedException, UnknownHostException {
-        final CouchbaseCore core = new CouchbaseCore();
-
-        String testBucket = "testInsertRemoveBucket" + TestProperties.bucket();
-        final StringBuilder sb = new StringBuilder();
-        sb.append("name=").append(testBucket);
-        sb.append("&ramQuotaMB=100&authType=sasl&saslPassword=").append(TestProperties.password());
-        sb.append("&replicaNumber=0&proxyPort=0&bucketType=membase&flushEnabled=0");
-
-        SeedNodesRequest seedNodes = new SeedNodesRequest(Arrays.asList(TestProperties.seedNode()));
-        OpenBucketRequest openDefault = new OpenBucketRequest(TestProperties.bucket(), TestProperties.password());
-        InsertBucketRequest insertAdhoc = new InsertBucketRequest(sb.toString(), TestProperties.adminUser(), TestProperties.adminPassword());
-        OpenBucketRequest openAdhoc = new OpenBucketRequest(testBucket, TestProperties.password());
-        RemoveBucketRequest removeAdhoc = new RemoveBucketRequest(testBucket, TestProperties.adminUser(), TestProperties.adminPassword());
-        CloseBucketRequest closeAdhoc = new CloseBucketRequest(testBucket);
-        CloseBucketRequest closeDefault = new CloseBucketRequest(TestProperties.bucket());
-
-        assertEquals("Error seeding nodes", ResponseStatus.SUCCESS, core.send(seedNodes).toBlocking().single().status());
-        assertEquals("Error opening default bucket", ResponseStatus.SUCCESS, core.send(openDefault).toBlocking().single().status());
-        assertEquals("Error inserting adhoc", ResponseStatus.SUCCESS, core.send(insertAdhoc).toBlocking().single().status());
-        Thread.sleep(2000);
-        assertEquals("Error opening adhoc", ResponseStatus.SUCCESS, core.send(openAdhoc).toBlocking().single().status());
-        assertEquals("Error closing adhoc", ResponseStatus.SUCCESS, core.send(closeAdhoc).toBlocking().single().status());
-
-        //reproduces the work done by clusterManager before delete bucket
-        //only useful if you close both buckets before the remove, as then node list will be empty
-        final InetAddress seedAddress = InetAddress.getByName(TestProperties.seedNode());
-        core.<AddNodeResponse>send(new AddNodeRequest(seedAddress))
-                        .flatMap(new Func1<AddNodeResponse, Observable<AddServiceResponse>>() {
-                            @Override
-                            public Observable<AddServiceResponse> call(AddNodeResponse response) {
-                                return core.send(new AddServiceRequest(ServiceType.CONFIG, TestProperties.adminUser(),
-                                        TestProperties.adminPassword(),8091, seedAddress));
-                            }
-                        })
-                .toBlocking().single();
-        assertEquals("Error removing adhoc", ResponseStatus.SUCCESS, core.send(removeAdhoc).toBlocking().single().status());
-
-        //moving this just below the closing of adhoc doesn't trigger the auth issue
-        assertEquals("Error closing default", ResponseStatus.SUCCESS, core.send(closeDefault).toBlocking().single().status());
-
-        core.send(new DisconnectRequest());
     }
 
 }
