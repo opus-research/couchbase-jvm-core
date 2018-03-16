823d5da8b2fc63ed1ac0357357553495af2a8bf9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:25:44 +0000

    First commited as after_52219_rev1

--- src/main/java/com/couchbase/client/core/endpoint/AbstractEndpoint.java
+++ src/main/java/com/couchbase/client/core/endpoint/AbstractEndpoint.java
@@ -147,6 +147,11 @@ public abstract class AbstractEndpoint extends AbstractStateMachine<LifecycleSta
     private volatile long reconnectAttempt = 1;
 
     /**
+     * Set to true once disconnected.
+     */
+    private volatile boolean disconnected;
+
+    /**
      * Preset the stack trace for the static exceptions.
      */
     static {
@@ -173,6 +178,7 @@ public abstract class AbstractEndpoint extends AbstractStateMachine<LifecycleSta
         this.responseBuffer = null;
         this.env = null;
         this.isTransient = isTransient;
+        this.disconnected = false;
     }
 
     /**
@@ -301,7 +307,16 @@ public abstract class AbstractEndpoint extends AbstractStateMachine<LifecycleSta
                             future.channel().eventLoop().schedule(new Runnable() {
                                 @Override
                                 public void run() {
-                                    doConnect(observable);
+                                    // Make sure to avoid a race condition where the reconnect could override
+                                    // the disconnect phase. If this happens, explicitly break the retry loop
+                                    // and re-run the disconnect phase to make sure all is properly freed.
+                                    if (!disconnected) {
+                                        doConnect(observable);
+                                    } else {
+                                        disconnect();
+                                        LOGGER.debug("{}Explicitly breaking retry loop because already disconnected.",
+                                            logIdent(channel, AbstractEndpoint.this));
+                                    }
                                 }
                             }, delay, delayUnit);
                         }
@@ -316,6 +331,8 @@ public abstract class AbstractEndpoint extends AbstractStateMachine<LifecycleSta
 
     @Override
     public Observable<LifecycleState> disconnect() {
+        disconnected = true;
+
         if (state() == LifecycleState.DISCONNECTED || state() == LifecycleState.DISCONNECTING) {
             return Observable.just(state());
         }
