082d2b7f8055f9de3ed439070ea321329d53336c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:27:03 +0000

    First commited as after_57957_rev8

---
+++ src/main/java/com/couchbase/client/core/message/kv/subdoc/multi/MutationCommand.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015 Couchbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
+ * IN THE SOFTWARE.
+ */
+
+package com.couchbase.client.core.message.kv.subdoc.multi;
+
+import com.couchbase.client.core.annotations.InterfaceAudience;
+import com.couchbase.client.core.annotations.InterfaceStability;
+import io.netty.buffer.ByteBuf;
+
+/**
+ * A single mutation description inside a TODO.
+ *
+ * @author Simon Baslé
+ * @since 1.2
+ */
+@InterfaceStability.Experimental
+@InterfaceAudience.Public
+public class MutationCommand {
+
+    private final Mutation mutation;
+    private final String path;
+    private final ByteBuf fragment;
+
+    /**
+     * Create a multi-mutation command.
+     *
+     * @param mutation the mutation type.
+     * @param path the path to mutate inside the document.
+     * @param fragment the target value for the mutation. This will be released when the request is sent.
+     */
+    public MutationCommand(Mutation mutation, String path, ByteBuf fragment) {
+        this.mutation = mutation;
+        this.path = path;
+        this.fragment = fragment;
+    }
+}
