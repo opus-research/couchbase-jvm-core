cf575b8afbd95f459f95c854d34dacd4715d4d06
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:23:59 +0000

    First commited as before_43782_rev19

--- src/main/java/com/couchbase/client/core/service/ServiceFactory.java
+++ src/main/java/com/couchbase/client/core/service/ServiceFactory.java
@@ -42,8 +42,6 @@ public class ServiceFactory {
                 return new ConfigService(hostname, bucket, password, port, env, responseBuffer);
             case QUERY:
                 return new QueryService(hostname, bucket, password, port, env, responseBuffer);
-            case DCP:
-                return new DCPService(hostname, bucket, password, port, env, responseBuffer);
             default:
                 throw new IllegalArgumentException("Unknown Service Type: " + type);
         }
