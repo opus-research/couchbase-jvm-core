b3b32f96bbbd01cfbb76a7023b4b66476363073e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:25:59 +0000

    First commited as before_53493_rev3

--- src/main/java/com/couchbase/client/core/endpoint/AbstractGenericHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/AbstractGenericHandler.java
@@ -31,7 +31,6 @@ import com.couchbase.client.core.logging.CouchbaseLoggerFactory;
 import com.couchbase.client.core.message.CouchbaseRequest;
 import com.couchbase.client.core.message.CouchbaseResponse;
 import com.couchbase.client.core.message.ResponseStatus;
-import com.couchbase.client.core.metrics.NetworkLatencyMetricsIdentifier;
 import com.couchbase.client.core.service.ServiceType;
 import com.lmax.disruptor.EventSink;
 import io.netty.channel.Channel;
@@ -85,8 +84,6 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
      */
     private final Queue<REQUEST> sentRequestQueue;
 
-    private final Queue<Long> sentRequestTimings;
-
     /**
      * If this handler is transient (will close after one request).
      */
@@ -103,10 +100,6 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
     private REQUEST currentRequest;
 
     private DecodingState currentDecodingState;
-
-    private long currentOpTime;
-
-    private String remoteHostname;
 
     /**
      * Creates a new {@link AbstractGenericHandler} with the default queue.
@@ -133,7 +126,6 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
         this.currentDecodingState = DecodingState.INITIAL;
         this.isTransient = isTransient;
         this.traceEnabled = LOGGER.isTraceEnabled();
-        this.sentRequestTimings = new ArrayDeque<Long>();
     }
 
     /**
@@ -174,7 +166,6 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
         ENCODED request = encodeRequest(ctx, msg);
         sentRequestQueue.offer(msg);
         out.add(request);
-        sentRequestTimings.offer(System.nanoTime());
     }
 
     @Override
@@ -182,10 +173,6 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
         if (currentDecodingState == DecodingState.INITIAL) {
             currentRequest = sentRequestQueue.poll();
             currentDecodingState = DecodingState.STARTED;
-            if (currentRequest != null) {
-                currentOpTime = System.nanoTime() - sentRequestTimings.poll();
-            }
-
             if (traceEnabled) {
                 LOGGER.trace("{}Started decoding of {}", logIdent(ctx, endpoint), currentRequest);
             }
@@ -195,19 +182,6 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
             CouchbaseResponse response = decodeResponse(ctx, msg);
             if (response != null) {
                 publishResponse(response, currentRequest.observable());
-
-                if (currentDecodingState == DecodingState.FINISHED) {
-                    if (currentRequest != null && env() != null && env().networkLatencyMetricsCollector().isEnabled()) {
-                        NetworkLatencyMetricsIdentifier identifier = new NetworkLatencyMetricsIdentifier(
-                            remoteHostname,
-                            serviceType().toString(),
-                            currentRequest.getClass().getSimpleName(),
-                            response.status().toString()
-                        );
-                        env().networkLatencyMetricsCollector().record(identifier, currentOpTime);
-                    }
-                }
-
             }
         } catch (CouchbaseException e) {
             currentRequest.observable().onError(e);
@@ -274,7 +248,6 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         LOGGER.debug(logIdent(ctx, endpoint) + "Channel Active.");
-        remoteHostname = ctx.channel().remoteAddress().toString();
         ctx.fireChannelActive();
     }
 
@@ -327,8 +300,6 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
                 LOGGER.info("Exception thrown while cancelling outstanding operation: " + req, ex);
             }
         }
-
-        sentRequestTimings.clear();
     }
 
 
