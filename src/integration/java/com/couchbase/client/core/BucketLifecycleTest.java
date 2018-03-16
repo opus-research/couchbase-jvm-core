5cefe89eb837becee51b3f07777b7d71745c8c0b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:23:51 +0000

    First commited as before_43289_rev1

--- src/integration/java/com/couchbase/client/core/BucketLifecycleTest.java
+++ src/integration/java/com/couchbase/client/core/BucketLifecycleTest.java
@@ -22,16 +22,18 @@
 package com.couchbase.client.core;
 
 import com.couchbase.client.core.config.ConfigurationException;
+import com.couchbase.client.core.message.ResponseStatus;
 import com.couchbase.client.core.message.cluster.OpenBucketRequest;
 import com.couchbase.client.core.message.cluster.OpenBucketResponse;
 import com.couchbase.client.core.message.cluster.SeedNodesRequest;
 import com.couchbase.client.core.util.TestProperties;
 import org.junit.Test;
 import rx.Observable;
+
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Verifies basic bucket lifecycles like opening and closing of valid and invalid buckets.
@@ -48,7 +50,7 @@ public class BucketLifecycleTest {
         core.send(new SeedNodesRequest(Arrays.asList(TestProperties.seedNode())));
         OpenBucketRequest request = new OpenBucketRequest(TestProperties.bucket(), TestProperties.password());
         Observable<OpenBucketResponse> response = core.send(request);
-        assertTrue(response.toBlocking().single().status().isSuccess());
+        assertEquals(ResponseStatus.SUCCESS, response.toBlocking().single().status());
     }
 
     @Test(expected = ConfigurationException.class)
