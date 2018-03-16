26e2eb427dcb0d91dead6887a6bcc65d8b0f45a6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:29:18 +0000

    First commited as after_71803_rev1

--- src/main/java/com/couchbase/client/core/config/parser/BucketConfigParser.java
+++ src/main/java/com/couchbase/client/core/config/parser/BucketConfigParser.java
@@ -17,7 +17,7 @@ package com.couchbase.client.core.config.parser;
 
 import com.couchbase.client.core.CouchbaseException;
 import com.couchbase.client.core.config.BucketConfig;
-import com.couchbase.client.core.env.CoreEnvironment;
+import com.couchbase.client.core.env.BootstrapEnvironment;
 import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -43,7 +43,7 @@ public final class BucketConfigParser {
      * @param input the raw string input.
      * @return the parsed bucket configuration.
      */
-    public static BucketConfig parse(final String input, final CoreEnvironment env) {
+    public static BucketConfig parse(final String input, final BootstrapEnvironment env) {
         try {
             InjectableValues inject = new InjectableValues.Std()
                     .addValue("env", env);
