788e1c0ddc2c166ed7904c4d9de3cf0d274895fd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:26:48 +0000

    First commited as before_56643_rev1

--- src/test/java/com/couchbase/client/core/config/refresher/CarrierRefresherTest.java
+++ src/test/java/com/couchbase/client/core/config/refresher/CarrierRefresherTest.java
@@ -29,6 +29,7 @@ import com.couchbase.client.core.config.ClusterConfig;
 import com.couchbase.client.core.config.ConfigurationProvider;
 import com.couchbase.client.core.config.DefaultNodeInfo;
 import com.couchbase.client.core.config.NodeInfo;
+import com.couchbase.client.core.endpoint.ResponseStatusConverter;
 import com.couchbase.client.core.endpoint.kv.KeyValueStatus;
 import com.couchbase.client.core.env.CoreEnvironment;
 import com.couchbase.client.core.env.DefaultCoreEnvironment;
@@ -41,7 +42,6 @@ import io.netty.buffer.Unpooled;
 import io.netty.util.CharsetUtil;
 import org.junit.Test;
 import rx.Observable;
-
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -77,9 +77,7 @@ public class CarrierRefresherTest {
 
         when(config.name()).thenReturn("bucket");
         List<NodeInfo> nodeInfos = new ArrayList<NodeInfo>();
-        Map<String, Integer> ports = new HashMap<String, Integer>();
-        ports.put("direct", 11210);
-        nodeInfos.add(new DefaultNodeInfo(null, "localhost:8091", ports));
+        nodeInfos.add(new DefaultNodeInfo(null, "localhost:8091", new HashMap<String, Integer>()));
         when(config.nodes()).thenReturn(nodeInfos);
 
         ByteBuf content = Unpooled.copiedBuffer("{\"config\": true}", CharsetUtil.UTF_8);
@@ -111,9 +109,7 @@ public class CarrierRefresherTest {
 
         when(config.name()).thenReturn("bucket");
         List<NodeInfo> nodeInfos = new ArrayList<NodeInfo>();
-        Map<String, Integer> ports = new HashMap<String, Integer>();
-        ports.put("direct", 11210);
-        nodeInfos.add(new DefaultNodeInfo(null, "localhost:8091", ports));
+        nodeInfos.add(new DefaultNodeInfo(null, "localhost:8091", new HashMap<String, Integer>()));
         when(config.nodes()).thenReturn(nodeInfos);
 
         ByteBuf content = Unpooled.copiedBuffer("", CharsetUtil.UTF_8);
@@ -146,9 +142,7 @@ public class CarrierRefresherTest {
         BucketConfig bucketConfig = mock(BucketConfig.class);
         when(bucketConfig.name()).thenReturn("bucket");
         List<NodeInfo> nodeInfos = new ArrayList<NodeInfo>();
-        Map<String, Integer> ports = new HashMap<String, Integer>();
-        ports.put("direct", 11210);
-        nodeInfos.add(new DefaultNodeInfo(null, "localhost:8091", ports));
+        nodeInfos.add(new DefaultNodeInfo(null, "localhost:8091", new HashMap<String, Integer>()));
         when(bucketConfig.nodes()).thenReturn(nodeInfos);
         Map<String, BucketConfig> bucketConfigs = new HashMap<String, BucketConfig>();
         bucketConfigs.put("bucket", bucketConfig);
@@ -185,10 +179,7 @@ public class CarrierRefresherTest {
         BucketConfig bucketConfig = mock(BucketConfig.class);
         when(bucketConfig.name()).thenReturn("bucket");
         List<NodeInfo> nodeInfos = new ArrayList<NodeInfo>();
-
-        Map<String, Integer> ports = new HashMap<String, Integer>();
-        ports.put("direct", 11210);
-        nodeInfos.add(new DefaultNodeInfo(null, "localhost:8091", ports));
+        nodeInfos.add(new DefaultNodeInfo(null, "localhost:8091", new HashMap<String, Integer>()));
         when(bucketConfig.nodes()).thenReturn(nodeInfos);
         Map<String, BucketConfig> bucketConfigs = new HashMap<String, BucketConfig>();
         bucketConfigs.put("bucket", bucketConfig);
@@ -225,11 +216,8 @@ public class CarrierRefresherTest {
         BucketConfig bucketConfig = mock(BucketConfig.class);
         when(bucketConfig.name()).thenReturn("bucket");
         List<NodeInfo> nodeInfos = new ArrayList<NodeInfo>();
-
-        Map<String, Integer> ports = new HashMap<String, Integer>();
-        ports.put("direct", 11210);
-        nodeInfos.add(new DefaultNodeInfo(null, "1.2.3.4:8091", ports));
-        nodeInfos.add(new DefaultNodeInfo(null, "2.3.4.5:8091", ports));
+        nodeInfos.add(new DefaultNodeInfo(null, "1.2.3.4:8091", new HashMap<String, Integer>()));
+        nodeInfos.add(new DefaultNodeInfo(null, "2.3.4.5:8091", new HashMap<String, Integer>()));
         when(bucketConfig.nodes()).thenReturn(nodeInfos);
         Map<String, BucketConfig> bucketConfigs = new HashMap<String, BucketConfig>();
         bucketConfigs.put("bucket", bucketConfig);
@@ -267,11 +255,8 @@ public class CarrierRefresherTest {
 
         when(config.name()).thenReturn("bucket");
         List<NodeInfo> nodeInfos = new ArrayList<NodeInfo>();
-
-        Map<String, Integer> ports = new HashMap<String, Integer>();
-        ports.put("direct", 11210);
-        nodeInfos.add(new DefaultNodeInfo(null, "1.2.3.4:8091", ports));
-        nodeInfos.add(new DefaultNodeInfo(null, "2.3.4.5:8091", ports));
+        nodeInfos.add(new DefaultNodeInfo(null, "1.2.3.4:8091", new HashMap<String, Integer>()));
+        nodeInfos.add(new DefaultNodeInfo(null, "2.3.4.5:8091", new HashMap<String, Integer>()));
         when(config.nodes()).thenReturn(nodeInfos);
 
         ByteBuf content = Unpooled.copiedBuffer("{\"config\": true}", CharsetUtil.UTF_8);
@@ -291,40 +276,5 @@ public class CarrierRefresherTest {
         assertEquals(0, content.refCnt());
     }
 
-    @Test
-    public void shouldIgnoreNodeWithoutKVServiceEnabled() throws Exception {
-        ClusterFacade cluster = mock(ClusterFacade.class);
-        ConfigurationProvider provider = mock(ConfigurationProvider.class);
-        BucketConfig config = mock(BucketConfig.class);
-
-        CarrierRefresher refresher = new CarrierRefresher(ENVIRONMENT, cluster);
-        refresher.provider(provider);
-
-        when(config.name()).thenReturn("bucket");
-        List<NodeInfo> nodeInfos = new ArrayList<NodeInfo>();
-
-        Map<String, Integer> ports = new HashMap<String, Integer>();
-        ports.put("direct", 11210);
-        nodeInfos.add(new DefaultNodeInfo(null, "1.2.3.4:8091", ports));
-        nodeInfos.add(new DefaultNodeInfo(null, "6.7.8.9:8091", new HashMap<String, Integer>()));
-        nodeInfos.add(new DefaultNodeInfo(null, "2.3.4.5:8091", ports));
-        when(config.nodes()).thenReturn(nodeInfos);
-
-        ByteBuf content = Unpooled.copiedBuffer("{\"config\": true}", CharsetUtil.UTF_8);
-        Observable<CouchbaseResponse> goodResponse = Observable.just((CouchbaseResponse) new GetBucketConfigResponse(
-                ResponseStatus.SUCCESS, KeyValueStatus.SUCCESS.code(),
-                "bucket",
-                content,
-                InetAddress.getByName("1.2.3.4")
-        ));
-        Observable<CouchbaseResponse> badResponse = Observable.error(new CouchbaseException("Failure"));
-        when(cluster.send(any(GetBucketConfigRequest.class))).thenReturn(badResponse, goodResponse);
-        refresher.markTainted(config);
-
-        Thread.sleep(1500);
-
-        verify(provider, times(1)).proposeBucketConfig("bucket", "{\"config\": true}");
-        assertEquals(0, content.refCnt());
-    }
 
 }
