e6db8f4bf09663acf568665876eef5ae5d261535
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:23:27 +0000

    First commited as after_39741_rev2

--- src/main/java/com/couchbase/client/core/message/query/GenericQueryResponse.java
+++ src/main/java/com/couchbase/client/core/message/query/GenericQueryResponse.java
@@ -3,20 +3,21 @@ package com.couchbase.client.core.message.query;
 import com.couchbase.client.core.message.AbstractCouchbaseResponse;
 import com.couchbase.client.core.message.CouchbaseRequest;
 import com.couchbase.client.core.message.ResponseStatus;
+import io.netty.buffer.ByteBuf;
 
 /**
  * Created by michael on 21/05/14.
  */
 public class GenericQueryResponse extends AbstractCouchbaseResponse {
 
-    private final String content;
+    private final ByteBuf content;
 
-    public GenericQueryResponse(String content, ResponseStatus status, CouchbaseRequest request) {
+    public GenericQueryResponse(ByteBuf content, ResponseStatus status, CouchbaseRequest request) {
         super(status, request);
         this.content = content;
     }
 
-    public String content() {
+    public ByteBuf content() {
         return content;
     }
 
