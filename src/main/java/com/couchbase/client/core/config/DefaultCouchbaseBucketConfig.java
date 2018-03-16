f1355f6229b0d9868e440d363d195405593ab4f0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:30:15 +0000

    First commited as before_76077_rev2

--- src/main/java/com/couchbase/client/core/config/DefaultCouchbaseBucketConfig.java
+++ src/main/java/com/couchbase/client/core/config/DefaultCouchbaseBucketConfig.java
@@ -24,7 +24,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -109,16 +108,13 @@ public class DefaultCouchbaseBucketConfig extends AbstractBucketConfig implement
         List<NodeInfo> partitionHosts = new ArrayList<NodeInfo>();
         for (String rawHost : partitionInfo.partitionHosts()) {
             InetAddress convertedHost;
-            int directPort;
             try {
-                String pair[] = rawHost.split(":");
-                convertedHost = InetAddress.getByName(pair[0]);
-                directPort = Integer.parseInt(pair[1]);
+                convertedHost = InetAddress.getByName(rawHost);
             } catch (UnknownHostException e) {
                 throw new ConfigurationException("Could not resolve " + rawHost + "on config building.");
             }
             for (NodeInfo nodeInfo : nodeInfos) {
-                if (nodeInfo.hostname().equals(convertedHost) && nodeInfo.services().get(ServiceType.BINARY) == directPort) {
+                if (nodeInfo.hostname().equals(convertedHost)) {
                     partitionHosts.add(nodeInfo);
                 }
             }
