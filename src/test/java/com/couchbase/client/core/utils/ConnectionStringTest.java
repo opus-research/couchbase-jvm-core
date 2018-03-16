5c566217ad65c86a336939975eb2307800c0e372
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:30:13 +0000

    First commited as before_76061_rev2

--- src/test/java/com/couchbase/client/core/utils/ConnectionStringTest.java
+++ src/test/java/com/couchbase/client/core/utils/ConnectionStringTest.java
@@ -112,15 +112,15 @@ public class ConnectionStringTest {
         ConnectionString parsed = ConnectionString.create("couchbase://user@localhost?foo=bar");
         assertEquals(ConnectionString.Scheme.COUCHBASE, parsed.scheme());
         assertEquals("user", parsed.username());
-        assertEquals(new InetSocketAddress("localhost", 0), parsed.hosts().get(0));
+        assertEquals(InetSocketAddress.createUnresolved("localhost", 0),parsed.hosts().get(0));
         assertEquals(1, parsed.params().size());
         assertEquals("bar", parsed.params().get("foo"));
 
         parsed = ConnectionString.create("couchbase://user123@host1,host2?foo=bar&setting=true");
         assertEquals(ConnectionString.Scheme.COUCHBASE, parsed.scheme());
         assertEquals("user123", parsed.username());
-        assertEquals(new InetSocketAddress("host1", 0), parsed.hosts().get(0));
-        assertEquals(new InetSocketAddress("host2", 0), parsed.hosts().get(1));
+        assertEquals(InetSocketAddress.createUnresolved("host1", 0),parsed.hosts().get(0));
+        assertEquals(InetSocketAddress.createUnresolved("host2", 0),parsed.hosts().get(1));
         assertEquals(2, parsed.params().size());
         assertEquals("bar", parsed.params().get("foo"));
         assertEquals("true", parsed.params().get("setting"));
