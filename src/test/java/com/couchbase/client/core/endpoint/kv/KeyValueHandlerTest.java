ec9b4c24eb8e3ce36ff787662d1a0455691ca0bd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:24:35 +0000

    First commited as before_45465_rev2

--- src/test/java/com/couchbase/client/core/endpoint/kv/KeyValueHandlerTest.java
+++ src/test/java/com/couchbase/client/core/endpoint/kv/KeyValueHandlerTest.java
@@ -21,7 +21,6 @@
  */
 package com.couchbase.client.core.endpoint.kv;
 
-import com.couchbase.client.core.CouchbaseException;
 import com.couchbase.client.core.endpoint.AbstractEndpoint;
 import com.couchbase.client.core.message.CouchbaseResponse;
 import com.couchbase.client.core.message.ResponseStatus;
@@ -122,7 +121,6 @@ public class KeyValueHandlerTest {
         assertEquals(1, outbound.getReserved());
         assertEquals(KeyValueHandler.OP_GET, outbound.getOpcode());
         assertEquals(0, outbound.getExtrasLength());
-        assertEquals(request.opaque(), outbound.getOpaque());
     }
 
     @Test
@@ -797,28 +795,6 @@ public class KeyValueHandlerTest {
         channel.writeInbound(response);
         assertEquals(1, content.refCnt());
         assertEquals(1, requestContent.refCnt());
+
     }
-
-    @Test(expected = CouchbaseException.class)
-    public void shouldFailWhenOpaqueDoesNotMatch() throws Exception {
-        ByteBuf content = Unpooled.copiedBuffer("content", CharsetUtil.UTF_8);
-        FullBinaryMemcacheResponse response = new DefaultFullBinaryMemcacheResponse("key", Unpooled.EMPTY_BUFFER,
-                content);
-        response.setStatus(BinaryMemcacheResponseStatus.SUCCESS);
-        response.setOpaque(1);
-
-        PrependRequest requestMock = mock(PrependRequest.class);
-        ByteBuf requestContent = Unpooled.copiedBuffer("content", CharsetUtil.UTF_8);
-        when(requestMock.bucket()).thenReturn("bucket");
-        AsyncSubject<CouchbaseResponse> responseSubject = AsyncSubject.<CouchbaseResponse>create();
-        when(requestMock.observable()).thenReturn(responseSubject);
-        when(requestMock.content()).thenReturn(requestContent);
-        when(requestMock.opaque()).thenReturn(3);
-        requestQueue.add(requestMock);
-
-        channel.writeInbound(response);
-        assertEquals(0, content.refCnt());
-        responseSubject.toBlocking().single();
-    }
-
 }
