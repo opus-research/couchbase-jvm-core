31f5d47f460f9d61c10edecfc0eba1b29adcc85e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:27:28 +0000

    First commited as before_60836_rev2

--- src/main/java/com/couchbase/client/core/node/locate/Locator.java
+++ src/main/java/com/couchbase/client/core/node/locate/Locator.java
@@ -21,17 +21,13 @@
  */
 package com.couchbase.client.core.node.locate;
 
-import com.couchbase.client.core.ResponseEvent;
 import com.couchbase.client.core.config.ClusterConfig;
-import com.couchbase.client.core.env.CoreEnvironment;
 import com.couchbase.client.core.message.CouchbaseRequest;
 import com.couchbase.client.core.node.Node;
-import com.lmax.disruptor.RingBuffer;
 
 import java.util.List;
 
 public interface Locator {
 
-    void locate(CouchbaseRequest request, List<Node> nodes, ClusterConfig config, CoreEnvironment env,
-                RingBuffer<ResponseEvent> responseBuffer);
+    Node[] locate(CouchbaseRequest request, List<Node> nodes, ClusterConfig config);
 }
