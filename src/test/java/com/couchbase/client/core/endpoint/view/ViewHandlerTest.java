ae236e7bb18aa8ee7ffb8c72aeeacc65e9dbc94c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:25:15 +0000

    First commited as after_48924_rev2

--- src/test/java/com/couchbase/client/core/endpoint/view/ViewHandlerTest.java
+++ src/test/java/com/couchbase/client/core/endpoint/view/ViewHandlerTest.java
@@ -480,7 +480,49 @@ public class ViewHandlerTest {
     }
 
     @Test
-    public void shoulDecodeNotFoundViewQueryResponse() {
+    @SuppressWarnings("unchecked")
+    public void shouldParseErrorWithEmptyRows() throws Exception {
+        String response = Resources.read("error_empty_rows.json", this.getClass());
+        HttpResponse responseHeader = new DefaultHttpResponse(HttpVersion.HTTP_1_1, new HttpResponseStatus(200, "OK"));
+        HttpContent responseChunk1 = new DefaultLastHttpContent(Unpooled.copiedBuffer(response, CharsetUtil.UTF_8));
 
+        ViewQueryRequest requestMock = mock(ViewQueryRequest.class);
+        queue.add(requestMock);
+        channel.writeInbound(responseHeader, responseChunk1);
+        latch.await(1, TimeUnit.SECONDS);
+        assertEquals(1, firedEvents.size());
+        ViewQueryResponse inbound = (ViewQueryResponse) firedEvents.get(0);
+        assertTrue(inbound.status().isSuccess());
+
+        List<ByteBuf> rows = inbound.rows().toList().toBlocking().single();
+        assertTrue(rows.isEmpty());
+
+        String error = inbound.error().toBlocking().single();
+        List<Object> parsed = mapper.readValue(error, List.class);
+        assertEquals(1, parsed.size());
     }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void shouldParseErrorAfterRows() throws Exception {
+        String response = Resources.read("error_rows.json", this.getClass());
+        HttpResponse responseHeader = new DefaultHttpResponse(HttpVersion.HTTP_1_1, new HttpResponseStatus(200, "OK"));
+        HttpContent responseChunk1 = new DefaultLastHttpContent(Unpooled.copiedBuffer(response, CharsetUtil.UTF_8));
+
+        ViewQueryRequest requestMock = mock(ViewQueryRequest.class);
+        queue.add(requestMock);
+        channel.writeInbound(responseHeader, responseChunk1);
+        latch.await(1, TimeUnit.SECONDS);
+        assertEquals(1, firedEvents.size());
+        ViewQueryResponse inbound = (ViewQueryResponse) firedEvents.get(0);
+        assertTrue(inbound.status().isSuccess());
+
+        List<ByteBuf> rows = inbound.rows().toList().toBlocking().single();
+        assertEquals(10, rows.size());
+
+        String error = inbound.error().toBlocking().single();
+        List<Object> parsed = mapper.readValue(error, List.class);
+        assertEquals(1, parsed.size());
+    }
+
 }
