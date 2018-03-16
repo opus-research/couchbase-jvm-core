54a4edec37226d23c626a241a37273ab92619482
Matheus Paixao <mhepaixao@gmail.com>
2018-03-16 12:26:16 +0000

    First commited as after_54961_rev1

--- src/test/java/com/couchbase/client/core/endpoint/util/ByteBufJsonHelperTest.java
+++ src/test/java/com/couchbase/client/core/endpoint/util/ByteBufJsonHelperTest.java
@@ -52,6 +52,16 @@ public class ByteBufJsonHelperTest {
         assertEquals(0, source.readerIndex());
     }
 
+    /**
+     * See JVMCBC-239.
+     */
+    @Test
+    public void testFindNextCharNotPrefixedByOnRealLifeExample() throws Exception {
+        ByteBuf source = Unpooled.copiedBuffer("123456\\\"78901234567890\",\n  \"signature\": ", CharsetUtil.UTF_8);
+
+        assertEquals(22, ByteBufJsonHelper.findNextCharNotPrefixedBy(source, '"', '\\'));
+    }
+
     @Test
     public void shouldFindSectionClosingPositionIfAtStartSection() throws Exception {
         ByteBuf source = Unpooled.copiedBuffer("{123}", CharsetUtil.UTF_8);
